ec65ac0166293ab60cb282d4184672687414fdae
Chris Luiz <chris@chrisluiz.io>
2017-03-18 13:27:52 -0600

    db model

---
+++ models.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+from app import db
+
+class Currency(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    exchange =db.Column(db.String())
+    price = db.Column(db.String())
+    hora = db.Column(db.DateTime())
+
+    def __init__(self, exchange, price, hora):
+        self.exchage = exchange
+        self.price = price
+        if hora is None:
+            hora = datetime.utcnow()
+        self.hora = hora
+
+    def __repr__(self):
+        return '<Currency {}>'.format(self.exchange)
